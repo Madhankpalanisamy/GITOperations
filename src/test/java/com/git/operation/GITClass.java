6f05761bca55c19462540f8fb255dffe4198cdc1
madhankpalanisamy <madhankpalanisamy@gmail.com>
2020-07-25 18:22:40 +0530

    maddy code commit

--- src/test/java/com/git/operation/GITClass.java
+++ src/test/java/com/git/operation/GITClass.java
@@ -4,8 +4,16 @@ public class GITClass {
 	
 	public void master() {
 		System.out.println("Master");
+		
+		
 	}
 
+	private void today() {
+		System.out.println("madhan got selected in hexaware");
+
+	}
+	
+	
 	public static void main(String[] args) {
 		
 	}
