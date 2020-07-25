537916e4dd4e1a365e8b01a4246c5a5f1e43925c
madhankpalanisamy <madhankpalanisamy@gmail.com>
2020-07-25 17:39:25 +0530

    sruthi code commit

--- src/test/java/com/git/operation/GITClass.java
+++ src/test/java/com/git/operation/GITClass.java
@@ -5,6 +5,10 @@ public class GITClass {
 	public void master() {
 		System.out.println("Master");
 	}
+	
+	public void sruthiBranch() {
+		System.out.println("Sruthi task completed!!!");
+	}
 
 	public static void main(String[] args) {
 		
